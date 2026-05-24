64df5fd3434c002e67a403ebb186151b895571be
Nithish <nithish@gmail.com>
2026-05-24 03:38:31 +0000

    Nithish test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("CI/CD Pipeline Working!");
+    res.send("Good luck Nithish!");
 });
 
 app.listen(3000, () => {
